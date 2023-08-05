a9eab994e6403983899365f81d0e67726d14d91b
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-08-05 19:29:06 -0400

    changed server code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 dotenv.config()
-import connectDB from './db.js'
+import connectDB from './db'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js';
@@ -19,15 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser())
 
-app.get('/', (req,res) => {
-    res.send("API is running")
-})
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
